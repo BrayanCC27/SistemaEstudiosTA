643814b50b3a73a01efa93320407ed6160b062c2
Brayan <bcclavijo27@gmail.com>
2025-08-19 16:30:20 -0500

    Modificar clase Persona: Estructura base

--- sistemaEstudios/src/main/java/Entidades/Persona.java
+++ sistemaEstudios/src/main/java/Entidades/Persona.java
@@ -1,4 +1,44 @@
 package Entidades;
 
 public class Persona {
+    private Double Id;
+    private String nombres;
+    private String apellidos;
+    private String email;
+
+    public String toString() {
+        return nombres + " " + apellidos;
+    }
+
+    public Double getId() {
+        return Id;
+    }
+
+    public void setId(Double id) {
+        Id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
